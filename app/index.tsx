e126f0089ddf4443d5352629df50f37a1e1a0716
TienPhuc02 <dtp2352002@gmail.com>
2023-10-27 15:58:56 +0700

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,34 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Page() {
+  return (
+    <View>
+      <View>
+        <Text className="text-red-400">Hello World</Text>
+        <Text>This is the first page of your app.</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 24,
+  },
+  main: {
+    flex: 1,
+    justifyContent: "center",
+    maxWidth: 960,
+    marginHorizontal: "auto",
+  },
+  title: {
+    fontSize: 64,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 36,
+    color: "#38434D",
+  },
+});
